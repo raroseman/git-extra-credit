a67793e7402e2dc6c93cd1a05bd47a0c3a0b8a18
wzhang-utsa <weining.zhang@utsa.edu>
2014-08-28 17:29:53 -0500

    Initial commit

--- README.md
+++
@@ -1,4 +0,0 @@
-git-extra-credit
-================
-
-For cs3443 students to get extra credit by practicing git
---
+++ src/extraCredit/Everybody.java
@@ -0,0 +1,9 @@
+package extraCredit;
+
+public class Everybody {
+
+	public static void main(String[] args) {
+		System.out.println("Instruction: Everybody add tow lines of output, separated by at least 24 hours.");
+	}
+
+}
