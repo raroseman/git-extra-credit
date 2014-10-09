6008ec66d8799097210f50b059c1a0bb76dc76bd
Brendan Nelson-Wess <qwk654@I3443C34.vdi.cs.lan>
2014-10-09 10:41:24 -0500

    added output

--- src/extraCredit/Everybody.java
+++ src/extraCredit/Everybody.java
@@ -5,6 +5,7 @@ public class Everybody {
 	public static void main(String[] args) {
 		System.out.println("Instruction: Everybody add tow lines of output, separated by at least 24 hours.");
         System.out.println("Kyle Haley, Edit: 10/09/2014 10:08AM");
+        System.out.println("Brendan Nelson-Weiss, Edit: 10/09/2014 10:39AM");
 	}
 
 }
