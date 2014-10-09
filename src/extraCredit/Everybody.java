f79fd0eff59e28fb5876e9e29e592c6e1b536724
gregoryhooks <gregoryhooksjr@gmail.com>
2014-10-09 18:57:20 -0500

    Added first print statement for Gregory Hooks

--- src/extraCredit/Everybody.java
+++ src/extraCredit/Everybody.java
@@ -6,6 +6,7 @@ public class Everybody {
 		System.out.println("Instruction: Everybody add tow lines of output, separated by at least 24 hours.");
         System.out.println("Kyle Haley, Edit: 10/09/2014 10:08AM");
         System.out.println("Brendan Nelson-Weiss, Edit: 10/09/2014 10:39AM");
+        System.out.println("Gregory Hooks, Edit: 10/09/2014 6:57PM")
 	}
 
 }
