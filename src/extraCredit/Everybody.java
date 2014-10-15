ca6565efad2c3506be45c789d55c1e817f292cf3
Isaias Puente <i.puente33@gmail.com>
2014-10-15 17:56:57 -0500

    Had to commit again to properly indent Everybody.java

--- src/extraCredit/Everybody.java
+++ src/extraCredit/Everybody.java
@@ -2,20 +2,20 @@ package extraCredit;
 
 public class Everybody {
 
-	public static void main(String[] args) {
-		System.out.println("Instruction: Everybody add tow lines of output, separated by at least 24 hours.");
-        System.out.println("Kyle Haley, Edit: 10/09/2014 10:08AM");
-        System.out.println("Brendan Nelson-Weiss, Edit: 10/09/2014 10:39AM");
-        System.out.println("Gregory Hooks, Edit: 10/09/2014 6:57PM");
-        System.out.println("Kyle Haley, Last Commit Message."
-               + " 10/12/2014 5:14 PM");
-        System.out.println("Gregory Hooks, Final Edit: 10/12/2014 5:31PM");
-        System.out.println("Zachary Rodriguez, Edit: 10/14/2014 10:43AM");
-        System.out.println("Eric Applonie 10/14/2014 11:30AM")
-        System.out.println("Kyle Sauriol 10/14/2014 1:23PM");
-        System.out.println("Michael Charo 10/14/2014 1342");
-        System.out.println("Laura Aquino 10/14/2014 1:44PM");
-        System.out.println("Laura Aquino 10/15/2014 2:37PM");
-	System.out.println("Isaias Puente 10/15/2014 2:47PM");
-	}
-}
+  public static void main(String[] args) {
+    System.out.println("Instruction: Everybody add tow lines of output, separated by at least 24 hours.");
+    System.out.println("Kyle Haley, Edit: 10/09/2014 10:08AM");
+    System.out.println("Brendan Nelson-Weiss, Edit: 10/09/2014 10:39AM");
+    System.out.println("Gregory Hooks, Edit: 10/09/2014 6:57PM");
+    System.out.println("Kyle Haley, Last Commit Message."
+		      + " 10/12/2014 5:14 PM");
+    System.out.println("Gregory Hooks, Final Edit: 10/12/2014 5:31PM");
+    System.out.println("Zachary Rodriguez, Edit: 10/14/2014 10:43AM");
+    System.out.println("Eric Applonie 10/14/2014 11:30AM");
+    System.out.println("Kyle Sauriol 10/14/2014 1:23PM");
+    System.out.println("Michael Charo 10/14/2014 1342");
+    System.out.println("Laura Aquino 10/14/2014 1:44PM");
+    System.out.println("Laura Aquino 10/15/2014 2:37PM");
+    System.out.println("Isaias Puente 10/15/2014 2:47PM");
+  }//end main
+}//end class
